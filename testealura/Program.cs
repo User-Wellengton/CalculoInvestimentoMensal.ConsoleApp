0a45d36a262f4e52cf1b33a3b13890ee7e24d265
User-Wellengton <wellengtonjm@gmail.com>
2022-02-11 17:10:40 -0300

    Add project files.

---
+++ testealura/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace testealura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double valorInvestido = 1000;
+            int totalMeses;
+            
+
+            Console.WriteLine("Quantos meses de investimento você quer saber ?");
+            totalMeses = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int cont = 1; cont <= totalMeses; cont ++ )
+                {
+               
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + cont + " , você tera : R$ " + valorInvestido);
+                  
+                }
+
+
+
+
+
+            
+        }
+    }
+}
